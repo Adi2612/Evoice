57db0351f51e8e2a7dd664f308dc192d1e8dfe10
Aaditya Arora <b17071@students.iitmandi.ac.in>
2019-10-16 11:46:55 +0530

    _festival Api init

--- _festival.cpp
+++ _festival.cpp
@@ -2,3 +2,10 @@
 * A python executable cpp wrap for C++ api defined for Festival 
 * 
 */
+#include <stdio.h>
+#include <festival.h>
+#include "festival_private.h"
+#include "Python.h"
+
+#define PyUnicodeObject23 PyUnicodeObject
+
